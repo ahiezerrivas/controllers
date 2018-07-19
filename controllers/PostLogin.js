3678457888c93aedf16adaac9b7d621a0cfadf67
Ahiezer <ahiezerrivas@gmail.com>
2018-07-18 20:42:03 -0400

    Los controlodadores de mi proyecto

---
+++ controllers/PostLogin.js
@@ -0,0 +1,165 @@
+
+//Post Login
+
+var mysql = require('mysql');
+
+var columna=null;
+
+var departamento=null;
+
+var columna_departamento = null;
+
+var UsuariosId = null;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proyecto'
+});
+
+connection.connect();
+
+
+exports.PostLogin = (req, res) => {
+          
+
+        var  Nombre = req.body.Nombre;
+
+        var Contraseña = req.body.password;
+
+        var message = '';
+
+        message = "Profile not found!";
+
+        connection.query("SELECT * FROM usuarios WHERE Nombre =? AND Contraseña=?", [Nombre, Contraseña], function (err, rows, fields)
+         {
+           if (err) throw err;
+
+           if (rows.length > 0) {
+
+               columna=rows;
+               UsuariosId= columna[0].idUsuarios
+               departamento=columna[0].Departamento_idDepartamento
+
+               console.log(columna);
+
+               console.log("se encontro");
+             
+
+               buscardepartamento();
+               buscarCargo();
+               buscarvideo();
+               res.redirect('/sesion/'+Nombre);
+               
+               
+           } else {
+                 
+               console.log("no se encontro");
+               res.render('index', {
+                   message:message
+               });
+           }
+
+        
+         })
+        
+    }
+
+
+
+
+
+function buscardepartamento() {
+    
+    connection.query('SELECT * FROM departamento WHERE idDepartamento = ?', [departamento], function (err, rows, fields) {
+        if (err) throw err;
+   
+        columna_departamento = rows;
+   
+   
+        console.log(columna_departamento);
+        
+   
+    });    
+
+}   ;
+
+
+var cargo = null;
+
+function buscarCargo() {
+
+    connection.query('SELECT * FROM cargos WHERE Usuarios_idUsuarios = ?', [UsuariosId], function (err, rows, fields) {
+        if (err) throw err;
+
+        cargo = rows;
+
+
+        console.log(cargo);
+        
+
+    });
+
+};
+
+
+
+
+  
+    exports.sesion = (req, res) => {
+
+    
+        
+    var Nombre= columna[0].Nombre;
+    console.log(Nombre)
+   
+    
+    buscarvideo();
+   
+   
+
+    res.render('sesion',{
+        columna:columna,
+        columna_departamento:columna_departamento,
+        cargo:cargo,
+        nombre:nombres
+    } );
+    
+   
+}
+
+
+
+var nombres = null;
+
+function buscarvideo() {
+
+    var sql = 'SELECT `Nombre` from videosencontrados';
+
+    var buscar = connection.query(sql, function buscar(err, rows, fields) {
+        if (err) throw err;
+        if (rows.length > 0) {
+            nombres = rows;
+            // console.log(nombres);
+
+        }
+
+
+    });
+}
+
+exports.enviar = (objeto) => {
+    
+     objeto.make=columna;
+        
+     
+}
+
+    , exports.nombres = (objeto) => {
+
+        objeto.make = nombres;
+
+
+    }
+
---
+++ controllers/video.js
@@ -0,0 +1,156 @@
+
+
+var PostLogin = require('./PostLogin');
+
+var usuario 
+
+
+        
+
+var mysql = require('mysql');
+
+var video = null;
+
+var message = null;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proyecto'
+});
+
+connection.connect();
+
+var Video=null
+
+var arreglo= null
+
+var palabra=new Array();
+
+var arreglo2 = [];
+
+ 
+
+var nombres=null;
+
+function buscarvideo() {
+
+    var sql = 'SELECT `Nombre` from videosencontrados';
+
+    var buscar = connection.query(sql, function buscar(err, rows, fields) {
+        if (err) throw err;
+        if (rows.length > 0) {
+            nombres = rows; 
+            // console.log(nombres);
+
+        }
+
+
+    });
+}
+    exports.Video = (req, res) => {
+        
+   
+     buscarvideo();
+    Video = req.body.Video;
+    arreglo =Video;
+    console.log(arreglo);
+    // let arreglo2= new Array();
+
+    var mycar = { make: "honda" },
+        columna;
+    
+    
+
+    PostLogin.enviar(mycar);
+
+    columna = mycar.make;
+
+   
+
+    //    for (let i = 0; i < Video.length; i++) {
+    //       let resultado= nombres.find(buscar => buscar.Nombre === Video[i]);
+    //       if(resultado !=null)
+    //       {
+    //           console.log(`Se encontro ${Video[i]}`);
+    //           arreglo2[i]=Video[i];
+    //       } 
+    //       else{
+    //           console.log(`No se encontro ${Video[i]}`);
+    //           arreglo2[i] = Video[i];
+    //       }
+    //    }       
+   
+
+
+    
+    
+
+    
+    
+    
+    res.render('prueba',{prueba:arreglo,columna:columna, nombre:nombres});
+    
+    
+   
+}
+
+
+exports.getSubir = (req, res) => {
+    var mycar = { make: "honda" },
+        columna;
+
+
+
+    PostLogin.enviar(mycar);
+
+    columna = mycar.make;
+
+
+    res.render('Subir',{columna:columna});
+
+
+}
+    
+exports.postSubir = (req, res) => {
+
+    var mycar = { make: "honda" },
+        columna;
+
+
+
+    PostLogin.enviar(mycar);
+
+    columna = mycar.make;
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    let sampleFile = req.files.sampleFile;
+    let translation = sampleFile.name;
+    if (translation.toLowerCase().endsWith('.mp4')) {
+        translation = translation.slice(0, -4)
+    }
+    
+    let Nombre = translation;
+    console.log(Nombre)
+    var sql = "INSERT INTO `videosencontrados`(`Nombre` ,`Url`) VALUES ('" + Nombre + "','" + sampleFile.name + "')";
+
+
+    sampleFile.mv('public/video/' + sampleFile.name, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        var query = connection.query(sql, function (err, result) {
+            if (err) throw err;
+
+            console.log("Se guardo");
+
+        });
+        res.render('Subir', { message: 'Se Guardo ', columna:columna });
+    });
+
+}
+
+   
+
